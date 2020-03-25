0925612047c5a9409a1894daab076f908cae9c07
Pranav Misal <pranav.misal@definelabs.in>
2020-03-25 18:56:24 +0530

    my changes add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import{RouterModule, Routes} from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
-import { CreateEmployeeComponent } from './employees/create-employee.component';
-import{FormsModule} from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ListEmployeesComponent } from "./employees/list-employees/list-employees.component";
+import { CreateEmployeeComponent } from "./employees/create-employee.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
-const appRoutes: Routes =[
-  {path:'list', component: ListEmployeesComponent},
-  {path:'create', component: CreateEmployeeComponent},
-  {path:'', redirectTo:'/list', pathMatch:'full'}
+const appRoutes: Routes = [
+  { path: "list", component: ListEmployeesComponent },
+  { path: "create", component: CreateEmployeeComponent },
+  { path: "", redirectTo: "/list", pathMatch: "full" }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListEmployeesComponent,
-    CreateEmployeeComponent
-  ],
+  declarations: [AppComponent, ListEmployeesComponent, CreateEmployeeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import{NgForm} from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Department } from "../models/department.model";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"]
 })
 export class CreateEmployeeComponent implements OnInit {
+  departments: Department[] = [
+    { id: 1, name: "Help Desk" },
+    { id: 2, name: "HR" },
+    { id: 3, name: "IT" },
+    { id: 4, name: "Payroll" },
+    { id: 4, name: "Admin" }
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  saveEmployee(empForm: NgForm): void {
+    console.log(empForm.value);
   }
-
-  saveEmployee(empForm: NgForm): void{
-    console.log(empForm);
-  }
-
 }
