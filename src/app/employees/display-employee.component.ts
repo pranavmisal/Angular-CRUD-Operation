e7900e229601f777c614757710f6b8c8a82256e0
Pranav Misal <pranav.misal@definelabs.in>
2020-03-27 17:26:50 +0530

    my changes

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Employee } from "../models/employee.model";
 
 @Component({
@@ -7,17 +7,7 @@ import { Employee } from "../models/employee.model";
   styleUrls: ["./display-employee.component.css"]
 })
 export class DisplayEmployeeComponent implements OnInit {
-  private _employee: Employee;
-
-  @Input()
-  set employee(val: Employee) {
-    console.log("Current:" + val.name);
-    console.log("Previous:" + (this._employee ? this._employee.name : "NULL"));
-    this._employee = val;
-  }
-  get employee(): Employee {
-    return this._employee;
-  }
+  @Input() employee: Employee;
 
   constructor() {}
 
